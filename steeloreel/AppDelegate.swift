ce48de5a47ddbcecd9c3877e317c30605d3babdd
Jessica Yao <jessicayao@jessicas-macbook-air.local>
2018-03-27 23:45:47 -0500

    login view controller screen working

--- steeloreel/AppDelegate.swift
+++ steeloreel/AppDelegate.swift
@@ -16,6 +16,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        //let isUserLoggedIn = UserDefaults.standard.bool(forKey: "isUserLoggedIn");
+//        if(true) {
+            let rootViewController: UIViewController = UIStoryboard(name: "LoginSignUp", bundle: nil).instantiateViewController(withIdentifier: "LoginSignUpViewController")
+            
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            appDelegate.window!.rootViewController = rootViewController
+            
+//        }
         return true
     }
 
