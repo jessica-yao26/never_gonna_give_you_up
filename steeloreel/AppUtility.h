090120490c31c183abaff638a3e9bdfb36c6c546
Jessica Yao <jessicayao@jessicas-macbook-air.local>
2018-03-28 22:39:54 -0500

    portrait lock working

---
+++ steeloreel/AppUtility.h
@@ -0,0 +1,13 @@
+//
+//  AppUtility.h
+//  steeloreel
+//
+//  Created by Jessica Yao on 3/28/18.
+//  Copyright © 2018 Jessica Yao. All rights reserved.
+//
+
+#ifndef AppUtility_h
+#define AppUtility_h
+
+
+#endif /* AppUtility_h */
--- steeloreel/LoginSignUpViewController.swift
+++ steeloreel/LoginSignUpViewController.swift
@@ -12,7 +12,7 @@ class LoginSignUpViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        AppUtility.lockOrientation(.portrait)
         // Do any additional setup after loading the view.
     }
 
